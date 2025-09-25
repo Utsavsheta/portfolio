25fb0d7737941263fda14b9fa4dd22b9e1c7f716
Utsav Sheta <utsavsheta5115@gmail.com>
2025-09-25 23:57:48 +0530

    - initial commit

---
+++ src/components/design/MyName.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="sr-only">Utsav Sheta</h1>
+  <h2 class="sr-only">Full-Stack Developer</h2>
+
+  <MyEnName id="svg-my-en-name" :class="$attrs.class" />
+</template>
+
+<script setup lang="ts">
+  import { MyEnName } from '..';
+</script>
